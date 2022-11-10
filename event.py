891cf41e2d8608dd522d5d612680c386cdf93124
Tejkin <Thomas1467@hotmail.com>
2022-11-10 14:34:45 +0800

    added second test event

--- event.py
+++ event.py
@@ -18,24 +18,53 @@ class Event():
         self.motion = None
 
     def __str__(self):
-        return f"In the {self.location} at {self.timestamp}\tMotion detection is {self.motion}\tPresence is {self.presence}"
+        """
+        Returns arguments in an appropriate, readable format
+        """
+        return f"In the {self.location} at {self.timestamp}\nMotion detection is {self.motion}\nPresence is {self.presence}"
     
     def set_motion(self, motion):
+        """
+        Motion parameter setter and also sets presence accordingly
+
+        Args:
+            motion (bool): motion detector response
+
+        Return
+            presence (bool): true if motion is true, false if motion is false
+        """
         self.motion = motion
+        if motion == True:
+            self.presence = True
+        else:
+            self.presence = False
 
     def get_motion(self):
-        return self.motion
-    
-    def set_presence(self):
-        if self.motion == True:
-            self.presence == True
-        else:
-            self.presence == False
+        """
+        Motion getter
 
+        Return:
+            motion (bool): motion detector response
+        """
+        return self.motion
+
+    def get_presence(self):
+        """
+        Presence getter
+
+        Return:
+            presence(bool): dependent on motion parameter
+        """
+        return self.presence
 
     
 if __name__ == '__main__':
     kitchen = Event("kitchen",)
     kitchen.set_motion(True)
 
+    bedroom = Event("bedroom")
+    bedroom.set_motion(False)
+
     print(kitchen.__str__())
+    print(bedroom.__str__())
+    pass
