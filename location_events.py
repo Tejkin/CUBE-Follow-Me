8a0afc451303544228a2be6552016fb15f6825b5
Tejkin <Thomas1467@hotmail.com>
2022-11-11 13:56:56 +0800

    made location event inherit from event

--- location_events.py
+++ location_events.py
@@ -1,7 +1,23 @@
 from event import Event
-class LocationEvents():
-    def __init__(self):
+class LocationEvent(Event):
+    def __init__(self, location):
+        super().__init__(location)
         self.all_motion = []
     
     def add_event(self, location):
         self.all_motion.append(location)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 from event import Event
-from location_events import LocationEvents
+from location_events import LocationEvent
 
 if __name__ == '__main__':
 
-    kitchen = Event("kitchen",)
-    bedroom = Event("bedroom")
-    living_room = Event("living room")
+    kitchen = LocationEvent("kitchen",)
+    bedroom = LocationEvent("bedroom")
+    living_room = LocationEvent("living room")
     
-    moved = LocationEvents()
+
 
     kitchen.set_motion(False)
     kitchen.set_motion(True)
 
-    for events in allEvents:
-        if events.check_presence_change() == True:
-            moved.add_event(events.get_location())
-            break
-        else:
-            break
-
     while True:
         if kitchen.check_presence_change() == True:
-            moved.add_event(kitchen.get_location())
+            kitchen.add_event(kitchen.get_location())
             break
         else:
             break
-    print(moved.all_motion)
+    print(kitchen.all_motion)
